faeb0b191166337fd8caa9897a06bd95103530a5
Luis Santos <lsantos.dev@gmail.com>
2022-08-17 23:53:19 +0100

    adding serverless YAML file

---
+++ handler.py
@@ -0,0 +1,28 @@
+import os
+
+from krakenapi import KrakenApi
+
+from krakendca.config import Config
+from krakendca.krakendca import KrakenDCA
+
+
+def main():
+    # Get parameters from configuration file.
+    current_directory: str = os.path.dirname(os.path.realpath(__file__))
+    config_file: str = current_directory + "/config.yaml"
+    config: Config = Config(config_file)
+    # Initialize the KrakenAPI object.
+    ka: KrakenApi = KrakenApi(config.api_public_key, config.api_private_key)
+    # Initialize KrakenDCA and handle the DCA based on configuration.
+    kdca: KrakenDCA = KrakenDCA(config, ka)
+    kdca.initialize_pairs_dca()
+    globals().update(locals())
+    kdca.handle_pairs_dca()
+
+
+def run(event, context):
+    main()
+
+
+#if __name__ == "__main__":
+#    main()
