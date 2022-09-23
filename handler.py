2b70d38897e307fcbd50351f6816efcde5953ae6
Luis Santos <lsantos.dev@gmail.com>
2022-09-23 19:49:45 +0100

    extend handler function to multiple config files

--- handler.py
+++ handler.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 
 from krakenapi import KrakenApi
 
@@ -7,17 +7,20 @@ from krakendca.krakendca import KrakenDCA
 
 
 def main():
-    # Get parameters from configuration file.
-    current_directory: str = os.path.dirname(os.path.realpath(__file__))
-    config_file: str = current_directory + "/config.yaml"
-    config: Config = Config(config_file)
-    # Initialize the KrakenAPI object.
-    ka: KrakenApi = KrakenApi(config.api_public_key, config.api_private_key)
-    # Initialize KrakenDCA and handle the DCA based on configuration.
-    kdca: KrakenDCA = KrakenDCA(config, ka)
-    kdca.initialize_pairs_dca()
-    globals().update(locals())
-    kdca.handle_pairs_dca()
+    curr_directory = Path(__file__).resolve().parent
+
+    # Iterate over the multiple configuration files
+    for config_file in curr_directory.glob("config*.yaml"):
+        # Read parameters from configuration file
+        config = Config(config_file)
+
+        # Initialize the KrakenAPI object
+        ka = KrakenApi(config.api_public_key, config.api_private_key)
+
+        # Initialize KrakenDCA and handle the DCA based on configuration
+        kdca = KrakenDCA(config, ka)
+        kdca.initialize_pairs_dca()
+        kdca.handle_pairs_dca()
 
 
 def run(event, context):
