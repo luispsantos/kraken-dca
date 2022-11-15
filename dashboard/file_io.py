cb3802f293b62f75630dc6af3db6c00b19f16b64
Luis Santos <lsantos.dev@gmail.com>
2022-11-15 15:52:47 +0000

    add code for dashboard application

---
+++ dashboard/file_io.py
@@ -0,0 +1,43 @@
+import json
+import os
+import time
+
+import pandas as pd
+
+
+def persist_to_file(file_name: str, ttl: int = 10 * 60):
+    """Decorator to persist return type of function to a file.
+
+    Only performs operation if the file does not exist or the TTL has expired.
+    """
+
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            cache_valid = True
+            try:
+                modification_time = os.path.getmtime(file_name)
+                if time.time() - modification_time > ttl:
+                    cache_valid = False
+
+                if cache_valid:
+                    with open(file_name, "r") as cache_file:
+                        if file_name.endswith(".json"):
+                            return json.load(cache_file)
+                        elif file_name.endswith(".csv"):
+                            return pd.read_csv(file_name)
+                        else:
+                            raise ValueError(f"Cannot process file {file_name}")
+            except (FileNotFoundError, IOError, ValueError):
+                pass
+
+            result = function(*args, **kwargs)
+            with open(file_name, "w") as cache_file:
+                if file_name.endswith(".json"):
+                    json.dump(result, cache_file)
+                elif file_name.endswith(".csv"):
+                    result.to_csv(file_name, index=False)
+            return result
+
+        return wrapper
+
+    return decorator
---
+++ dashboard/kraken.py
@@ -0,0 +1,43 @@
+from concurrent.futures import ThreadPoolExecutor
+from decimal import Decimal
+
+import boto3
+import pandas as pd
+import yaml
+from file_io import persist_to_file
+from krakenapi import KrakenApi
+
+kraken_api = KrakenApi()
+
+with open("pairs.yml", "r") as yml_file:
+    pairs = yaml.safe_load(yml_file)
+
+
+@persist_to_file("orders.csv")
+def get_order_history():
+    session = boto3.Session(profile_name="personal")
+    client = session.resource("dynamodb")
+    table = client.Table("kraken-dca")
+    response = table.scan()
+    for order in response["Items"]:
+        for key, val in order.items():
+            if isinstance(val, Decimal):
+                order[key] = float(val)
+    orders = pd.DataFrame(response["Items"])
+
+    return orders
+
+
+@persist_to_file("asset_prices.json")
+def get_asset_prices():
+    with ThreadPoolExecutor() as executor:
+        asset_prices = list(executor.map(get_asset_price, pairs))
+        asset_prices = dict(zip(pairs, asset_prices))
+
+    return asset_prices
+
+
+def get_asset_price(pair):
+    ticker_information = kraken_api.get_pair_ticker(pair)
+    pair_ask_price = float(ticker_information.get(pair).get("a")[0])
+    return pair_ask_price
