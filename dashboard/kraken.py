eba018d65c2fe289282e7d644619965d78443248
Luis Santos <lsantos.dev@gmail.com>
2022-11-18 18:47:57 +0100

    sort traded pairs by their total trading volume

--- dashboard/kraken.py
+++ dashboard/kraken.py
@@ -10,15 +10,12 @@ from file_io import persist_to_file
 
 kraken_api = KrakenApi()
 
-with open("pairs.yml", "r") as yml_file:
-    pairs = yaml.safe_load(yml_file)
-
 
 @persist_to_file("orders.csv")
-def get_order_history():
-    session = boto3.Session(profile_name="personal")
+def get_order_history(aws_profile="personal", orders_table="kraken-dca"):
+    session = boto3.Session(profile_name=aws_profile)
     client = session.resource("dynamodb")
-    table = client.Table("kraken-dca")
+    table = client.Table(orders_table)
     response = table.scan()
     for order in response["Items"]:
         for key, val in order.items():
@@ -30,7 +27,7 @@ def get_order_history():
 
 
 @persist_to_file("asset_prices.json")
-def get_asset_prices():
+def get_asset_prices(pairs):
     with ThreadPoolExecutor() as executor:
         asset_prices = list(executor.map(get_asset_price, pairs))
         asset_prices = dict(zip(pairs, asset_prices))
@@ -42,3 +39,9 @@ def get_asset_price(pair):
     ticker_information = kraken_api.get_pair_ticker(pair)
     pair_ask_price = float(ticker_information.get(pair).get("a")[0])
     return pair_ask_price
+
+
+def load_all_pairs():
+    with open("all_pairs.yml", "r") as yml_file:
+        all_pairs = yaml.safe_load(yml_file)
+    return all_pairs
